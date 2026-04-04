0810f94329dc8ce2c1961d2ccf8f9a5b2ba2f322
Frogger25 <gavinhanzel@gmail.com>
2026-04-04 14:45:03 -0500

    Changing to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Friends from "./Pages/Friends";
 import Messages from "./Pages/Messages";
 import Login from "./Pages/Login";
